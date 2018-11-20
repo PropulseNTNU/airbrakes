13b822a4ed7c9eafb517df127c80f667c8116408
Herman Dragesund <hermandr@stud.ntnu.no>
2018-11-20 10:43:02 +0000

    PIDcontroller

--- Code/airbrakes/airbrakes.ino
+++ Code/airbrakes/airbrakes.ino
@@ -2,7 +2,7 @@
 #include "airbrakes_sensors.h"
 #include "airbrakes_setup.h"
 #include "rocket.h"
-
+#include "controll.h"
 // SPI library is included for communication between the airbrakes and fc controllers
 #include <SPI.h>
 
@@ -16,13 +16,32 @@ int val = 0;
 void setup() {
   rocket = Rocket::Instance();
   // initialize SPI
-  SPI.begin(); 
+  SPI.begin();
+
 }
-
+float riemann_sum=0;
+long timestep = 0;
+unsigned long time_start=millis();
+float cur_velocity=rocket->getVelocity();
 void loop() {
-  val = analogRead(potPin);
-  val =  map(val, 0, 1023, 0, 180);
-  rocket->setAirbraksesPosition(val);
-  delay(40);
-
+  timestep=millis()-time_start;
+  time_start=millis();
+  float prev_velocity=cur_velocity;
+  cur_velocity=rocket->getVelocity();
+  //val = analogRead(potPin);
+  //val =  map(val, 0, 1023, 0, 180);
+  //rocket->setAirbraksesPosition(val);
+  //delay(40);
+  float  u= controller(calk_ref(rocket->getAltitude()), rocket->getVelocity(), timestep, &riemann_sum, cur_velocity, prev_velocity, 5,5,5);
+  prev_velocity=cur_velocity;
+  if (u>180) {
+    rocket->setAirbraksesPosition(180);
+  }
+  else if (u<0) {
+    rocket->setAirbraksesPosition(0);
+  }
+  else{
+    rocket->setAirbraksesPosition(u);
+  }
+  Serial.print(u);
 }
---
+++ Code/airbrakes/controll.h
@@ -0,0 +1,8 @@
+//variables
+extern float riemann_sum;
+
+//functions
+float calk_ref(int altitude);
+float controller(int reference, int velocity, long step, float *riemann_sum, float cur_velocity, float prev_velocity, int kpp, int kpd, float kpi);
+float integrate(float prev_sum, float value, long step);
+float deriver(float prev_val, float cur_val, long step); 
