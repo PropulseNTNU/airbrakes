09fdbc56ab5b637de9e6307c0a3ea63fbd164534
Herman Dragesund <hermandr@stud.ntnu.no>
2019-02-19 09:09:57 +0100

    minor changes

--- airbrakes.ino
+++ airbrakes.ino
@@ -9,15 +9,15 @@ Servo _servo;
 //initilises variables
 float error = 0; //error used in controller
 float riemann_sum = 0; //used in integrator, witch is used in controller
-float u = 45; //sets servo to 45 degrees, this causes the air brakes to brake at 50% capasaty
-float prev_u=45;//only used for testing
+float u = 0; //sets servo to 45 degrees, this causes the air brakes to brake at 50% capasaty
+float prev_u=0;//only used for testing
 float dt = 0; //time step used in integrator and kalman filter
 Parameters parameters = { 1 , 1 , 1 }; //Control parameters (Kp, Ki, Kd)
 unsigned long time_new, time_old = 0; // time variables for delta time
 
 float sensor_data[2]={0,0}; //Barometer at index 0 and accelrometer (z-direction)at index 1. Utvides kanskje senere m/pitch
 float estimates[2]; //Estimates from Kalman filter. [height, velocity]
-float reference_v= 200; //reference_velovity
+float reference_v= 0; //reference_velovity
 
 void setup() { //initiates servo and printing
   pinMode(SERVO_PIN, OUTPUT);
@@ -70,6 +70,12 @@ void loop() { //Main-loop. Will be replaced with the loop in the statemachine.
   time_old = time_new;
   prev_u=test_modifications(u, prev_u, dt);
   u=prev_u;
+  if (u<90){
+    u=90;
+  }
+  if (u<0){
+    u=0;
+  }
   Serial.print("c_s");
   Serial.println(test_calculate_area(u));
    //Serial.print("dt:");
