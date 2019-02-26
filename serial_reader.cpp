1507635dc203e13f5d32d476b5a2c9e4e4306d1c
havard <hloevi@live.no>
2019-02-26 21:49:28 +0100

    Fix - new reading method

---
+++ serial_reader.cpp
@@ -0,0 +1,69 @@
+
+#include <string.h>
+#include <stdlib.h> 
+#include <Arduino.h>
+const int numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+
+      // variables to hold the parsed data
+char messageFromPC[numChars] = {0};
+
+bool newData = false;
+
+void parseData(float * sensor_data) {      // split the data into its parts
+
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+ 
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    sensor_data[0] = atof(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ",");
+    sensor_data[1] = atof(strtokIndx);     // convert this part to a float
+}
+
+void recvWithStartEndMarkers() {
+    static bool recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void updateSensorData(float * sensor_data){
+  recvWithStartEndMarkers();
+  if (newData == true) {
+      strcpy(tempChars, receivedChars);
+          // this temporary copy is necessary to protect the original data
+          //   because strtok() used in parseData() replaces the commas with \0
+      parseData(sensor_data);
+      newData = false;
+  }
+}
---
+++ serial_reader.h
@@ -0,0 +1,10 @@
+#ifndef SERIAL_READER_H
+#define SERIAL_READER_H
+#include <BasicLinearAlgebra.h>
+
+void parseData();
+void recvWithStartEndMarkers();
+void updateSensorData(float* sensor_data);
+
+
+#endif
